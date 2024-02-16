b9c314d217a3a4d6d80b17f81f0015cc1116c615
Daniel Billingsley <d.billingsley@eagles.oc.edu>
2024-02-15 20:51:34 -0600

    update(v1): made rnable multiply and helloworld

--- scramble.py
+++ scramble.py
@@ -5,7 +5,7 @@ readLines = []
 try: 
     fileName = sys.argv[1]
     file = open(fileName)
-    readLines  = file.read().split("\n")
+    readLines = file.read().split("\n")
     file.close()
 except Exception as e:
     print(f"File error.\n{e}")
@@ -16,70 +16,61 @@ count = 0
 dinnerPlate = ''
 
 def err(str):
-   print("\n" + str + f" at line {count}")
-   sys.exit(0)
+    print("\n" + str + f" at line {count}")
+    sys.exit(0)
 
-def pop(index = -1):
+def pop(index=-1):
     if len(stack) < 1:
         err("Error: Stack underflow")
     return stack.pop(index)
 
 while count >= 0 and count < len(readLines):
     parts = readLines[count].split(" ")
-    sub = parts[0]
+    cmd = parts[0]
 
-    # push value -> CRACK
-    if readLines[0] == 'CRACK':
-        stack.append(readLines[1])
-    # pop value -> SERVE
-    elif readLines[0] == 'SERVE':
-        print(pop())
-    # add top 2 values -> BEAT
-            # TURN THE BEAT AROUND (google "taeb")
-    elif readLines[0] == 'BEAT':
+    if cmd == 'CRACK':
+        stack.append(int(parts[1]))  # push value
+    elif cmd == 'SERVE':
+        print(pop())  # pop value
+    elif cmd == 'BEAT':
         x = pop()
-        stack[-1] += x
-    # subtract top 2 values -> STRAIN
-    elif readLines[0] == 'STRAIN':
+        stack[-1] += x  # add top 2 values
+    elif cmd == 'STRAIN':
         x = pop()
-        stack[-1] -= x
-    # multiply top 2 values -> MAKEANOMELETTE
-    elif readLines[0] == 'MAKEANOMELETTE':
+        stack[-1] -= x  # subtract top 2 values
+    elif cmd == 'MAKEANOMELETTE':
         x = pop()
-        stack[-1] *= x
-    # divide top 2 values -> SCRAMBLE
-    elif readLines[0] == 'SCRAMBLE':
+        stack[-1] *= x  # multiply top 2 values
+    elif cmd == 'SCRAMBLE':
         x = pop()
-        readLines[-1] /= x
-    # leave program -> EAT
-    elif readLines[0] == 'EAT':
-        break
-    # copy top value -> INCUBATE
-    elif readLines[0] == 'INCUBATE':
-        readLines.append(stack[-1])
-    # switch top 2 values -> FLIP
-    elif readLines[0] == 'FLIP':
+        stack[-1] /= x  # divide top 2 values
+    elif cmd == 'EAT':
+        break  # leave program
+    elif cmd == 'INCUBATE':
+        stack.append(stack[-1])  # copy top value
+    elif cmd == 'FLIP':
         x = pop()
         y = pop()
         stack.append(x)
-        stack.append(y)
-    # pop top as ASCII -> FRY
-    elif readLines[0] == 'FRY':
-        print(chr(pop()))
-    # pop top as int -> POACH
-    elif readLines[0] == 'POACH':
-        print(int(pop()))
-    # drop stack -> BURN
-    elif readLines[0] == 'BURN':
-        stack = []
-    # repeat top value n times as ascii -> BATCH
-    elif readLines[0] == 'BATCH':
-        for i in readLines[1]:
-            stack.append(chr(stack[-1]))
-    # repeat top value n times as int -> OVEREASY
-    elif readLines[0] == 'OVEREASY':
-        for i in readLines[1]:
-            stack.append(int(stack[-1]))
-    count + 1
+        stack.append(y)  # switch top 2 values
+    elif cmd == 'FRY':
+        print(chr(pop()))  # pop top as ASCII
+    elif cmd == 'POACH':
+        print(int(pop()))  # pop top as int
+    elif cmd == 'BURN':
+        stack = []  # drop stack
+    elif cmd == 'BATCH':
+        for i in range(int(parts[1])):
+            stack.append(chr(stack[-1]))  # repeat top value n times as ASCII
+    elif cmd == 'OVEREASY':
+        for i in range(int(parts[1])):
+            stack.append(int(stack[-1]))  # repeat top value n times as int
+    elif cmd == "EAT":
+      try:
+         stack.append(ord(input("")[0]))   
+      except IndexError:
+         stack.append(0)
+
+    count += 1
 
 print('')
